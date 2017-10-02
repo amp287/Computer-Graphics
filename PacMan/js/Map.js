4ab682bc5d86c1fa6bbb96956d5e9dd2b5135c24
Unknown <guitar12amp@aol.com>
2017-10-02 19:09:05 -0400

    Pac-Man:Pointer Lock working

--- PacMan/js/Map.js
+++ PacMan/js/Map.js
@@ -1,5 +1,5 @@
-var map = {
-    "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", 
+var map = 
+   ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", 
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
@@ -19,5 +19,49 @@ var map = {
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
     "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", 
-    "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", 
+    "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"
+];
+
+var groundPlane;
+
+function generateMap(){
+        var material = new Physijs.createMaterial(new THREE.MeshLambertMaterial({color:"white"}), .95, 1.3 );
+		var geometry = new THREE.PlaneGeometry( 300, 300, 6 );
+		groundPlane = new Physijs.BoxMesh( geometry, material, 0 );
+		groundPlane.name = "GroundPlane";
+		
+		scene.add( groundPlane );
+    
+    var height = 10;
+    var width = 5;
+    var length = 5;
+    
+    geometry = new THREE.CubeGeometry(width, height, length);
+    material = new Physijs.createMaterial(new THREE.MeshLambertMaterial(), .95, 1.3);
+    
+    var cube = new Physijs.BoxMesh(geometry, material, 0);
+    
+    var leftTop = groundPlane.position.clone();
+    
+    leftTop.x += 150;
+    leftTop.y -= 150;
+    leftTop.z = 7;
+    
+    for(i = 0; i < map.length; i++){
+        var cube;
+        if(map[i] == "1"){
+            cube = new Physijs.BoxMesh(geometry, material, 0);
+            cube.position.set(leftTop.x, leftTop.y, leftTop.z);
+            cube.position.x += leftTop.x * (width * i % 20);
+            cube.position.y += leftTop.y * (width * i % 20);
+            
+            cube.name = "Wall";
+            
+            scene.add(cube);
+        }
+    }
+    
+    
+    
+    
 }
